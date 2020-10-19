4426f68b896ec88ba2e8913fa69137687eb8b3b8
Shehab Hosny <shehab2015@live.com>
2020-10-20 01:00:40 +0200

    Update ADC.c

--- ADC.c
+++ ADC.c
@@ -6,19 +6,19 @@ void ADC0_Init()
 {
 	GPIO_PORTE_DIR_R   &= ~0x08;						// Input of Throttle is PE3 Pin
 	GPIO_PORTE_DEN_R   &= ~0x08;						// Disable Digital I/O on PE4
-	GPIO_PORTE_AFSEL_R |= 0x08;							// Enable Alternative Function to Pin PE4
-	GPIO_PORTE_AMSEL_R |= 0x08;							// Enable Analog Function to Pin PE4
+	GPIO_PORTE_AFSEL_R |= 0x08;						// Enable Alternative Function to Pin PE4
+	GPIO_PORTE_AMSEL_R |= 0x08;						// Enable Analog Function to Pin PE4
 	
-	SYSCTL_RCGC0_R  |= 0x10000;							// Activate ADC0 module
-	SYSCTL_RCGC0_R  &= ~0x0300;							// Set the ADC0 Sampling Speed to 125K Samples/Second
+	SYSCTL_RCGC0_R  |= 0x10000;						// Activate ADC0 module
+	SYSCTL_RCGC0_R  &= ~0x0300;						// Set the ADC0 Sampling Speed to 125K Samples/Second
 	
-	ADC0_SSPRI_R 	 = 0x0123;								// Sample Sequencer 3 (SS3) is highest priority
-	ADC0_ACTSS_R	&= ~0x08;									// Disable Sample Sequencer 3 (SS3)
-	ADC0_EMUX_R		&= ~0xF000;
+	ADC0_SSPRI_R 	 = 0x0123;						// Sample Sequencer 3 (SS3) is highest priority
+	ADC0_ACTSS_R	&= ~0x08;						// Disable Sample Sequencer 3 (SS3)
+	ADC0_EMUX_R	&= ~0xF000;
 	ADC0_SSMUX3_R	&= ~0x000F;
-	ADC0_SSMUX3_R += 0;
-	ADC0_SSCTL3_R = 0x0006;
-	ADC0_IM_R &= ~0x0008;
+	ADC0_SSMUX3_R 	+= 0;
+	ADC0_SSCTL3_R 	= 0x0006;
+	ADC0_IM_R &	= ~0x0008;
 	ADC0_ACTSS_R	|= 0x08;
 	
 	ADC0_PSSI_R = 0x0008;
